8c4eea6ff55c1fdb42ab462761e2cf36f6ae6376
Anabel Sposato <asposato@Brunos-MacBook-Pro.local>
2017-04-13 09:56:33 +0100

    initial project version

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+/**
+ * Created by asposato on 12/04/2017.
+ */
+
+
+function Car( brand, model, year, cost, dimensions, fuelEconomy, kerbWeight, tankCapacity ) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.cost = cost;
+    this.dimensions = dimensions;
+    this.fuelEconomy = fuelEconomy;
+    this.kerbWeight = kerbWeight;
+    this.tankCapacity = tankCapacity;
+}
+
+
+var au
+
+
+
+
+/*
+Audi A4
+ RRP: From £26,265
+ Dimensions: 4,725-4,726 mm L x 1,842 mm W x 1,427-1,434 mm H
+ Fuel economy: 45-74 mpg combined (36-63 city, 52-83 highway)
+ Kerb weight: 1,320 to 1,695 kg
+ Fuel tank capacity: 40 to 58 L
+*/
+
+
+/*
+
+ Mercedes C-Class
+
+ RRP: From £28,160
+ Dimensions: 4,686-4,771 mm L x 1,810-1,877 mm W x 1,400-1,467 mm H
+ Fuel economy: 32-79 mpg combined (23-72 city, 39-83 highway)
+ Acceleration 0-62 mph: 3.9 to 10.6 seconds
+ Max speed: 76.43 to 154.7 mph
+
+ */
+
+/*
+ BMW 3 Series
+
+ RRP: From £24,800
+ Dimensions: 4,633-4,671 mm L x 1,811-1,877 mm W x 1,424-1,467 mm H
+ Fuel economy: 32-72 mpg combined (23-60 city, 41-83 highway)
+ Kerb weight: 1,475 to 1,770 kg
+ Acceleration 0-62 mph: 4.2 to 11.2 seconds
+ */
+
+
+/*  Jaguar XE
+ RRP: From £27,525
+ Dimensions: 4,672 mm L x 1,850 mm W x 1,416-1,425 mm H
+ Fuel economy: 35-74 mpg combined (24-64 city, 46-83 highway)
+ Kerb weight: 1,435 to 1,655 kg
+ Acceleration 0-62 mph: 5 to 8.4 seconds
+*/
